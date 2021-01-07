e0e59ce71e97941dcd903091a99c5bc87d26f2f5
Soham Panja <76804785+sohampanja89@users.noreply.github.com>
2021-01-07 20:36:59 +0530

    HttpservletRequest

--- SpringMVC_Demo/src/main/java/com/soham/myApp/AddController.java
+++ SpringMVC_Demo/src/main/java/com/soham/myApp/AddController.java
@@ -3,10 +3,8 @@ package com.soham.myApp;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
